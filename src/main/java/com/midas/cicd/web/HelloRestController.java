6ee35ac802b315ad2cd29c062696359cd6d04d43
midasworld <midas117@knou.ac.kr>
2022-07-13 16:45:06 +0900

    feat: Hello 컨트롤러 생성

---
+++ src/main/java/com/midas/cicd/web/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.midas.cicd.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloRestController {
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+}
